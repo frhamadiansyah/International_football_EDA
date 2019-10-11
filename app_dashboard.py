471a2cf3e2a077a83edfd9ce55f07d439dc1ab44
frhamadiansyah <fandrian.rhamadiansyah@gmail.com>
2019-10-11 16:41:30 +0700

    add several new tab

--- app_dashboard.py
+++ app_dashboard.py
@@ -5,41 +5,62 @@ import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output, State
-from src.graph import matchresult
+from src.graph import matchresult, homeawayrecord, rivalresult, showrivaldataframe
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWlwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
 
 football = pd.read_csv('football.csv')
+football['date'] = pd.to_datetime(football['date'])
 
 app.layout = html.Div(children=[
-    html.H1('Football Dashboard', style = {'text-align' : 'center','padding' : '25px'}),
+    html.H1('International Football Dashboard', style = {'text-align' : 'center','padding' : '25px'}),
     html.P('Created by Fandri', style = {'text-align' : 'center'}),
 
     html.Div(children = [
-        html.P('Choose country : ', className = 'col-3',style = {'margin-left' : '25px'}),
-        html.P('n-year : ', className = 'col-3',style = {'margin-left' : '25px'})
+        html.P('Choose country : ', className = 'col-3',style = {'margin-left' : '0px'}),
+        html.P('n-year : ', className = 'col-3',style = {'margin-left' : '0px'})
         ], className = 'row'),
     html.Div(children = [
-        html.Div(dcc.Input(id='choose-country', value='Indonesia', type='text'),className = 'col-3', style = {'margin-left' : '25px'}),
-        html.Div(dcc.Input(id='n-year', value=15, type='number'),className = 'col-3', style = {'margin-left' : '25px'})
+        html.Div(dcc.Input(id='choose-country', value='Indonesia', type='text'),className = 'col-3', style = {'margin-left' : '0px'}),
+        html.Div(dcc.Input(id='n-year', value=15, type='number'),className = 'col-3', style = {'margin-left' : '0px'})
         ], className = 'row'),
     ## Add search button
-    html.Div(html.Button('Search', id = 'search'), style = {'padding' : '15px'}),
+    html.Div(html.Button('Search', id = 'search'), style = {'margin-top' : '25px', 'margin-bottom' : '25px'}),
     ## Add tab
     dcc.Tabs(value = 'tabs', id = 'tabs-1',children = [
         dcc.Tab(label= 'Match Result', value = 'tab-nol', children = [
             ## Add graph
             html.Div(children = dcc.Graph(
-                id='graph-scatter',
+                id='total-bar-chart',
                 figure = matchresult(football, 'Indonesia', 15)
             ), className = 'col-12')
         ]
         ),
-        dcc.Tab(label= 'Home-record', value = 'tab-satu', children = [
+        dcc.Tab(label= 'location-record', value = 'tab-satu', children = [
             ## Add graph
-            html.P('lalala')
+            html.Div(children = dcc.Graph(
+                id='pie-chart',
+                figure=homeawayrecord(football, 'Indonesia', 15)
+            ), className = 'col-12')
+        ]
+        ),
+        dcc.Tab(label= 'rival-tab', value = 'tab-dua', children = [
+            ## Add graph
+            html.Div(children = dcc.Graph(
+                id='rival-chart',
+                figure=rivalresult(football, 'Indonesia')
+            ), className = 'col-12')
+        ]
+        ),
+        dcc.Tab(label= 'head-to-head', value = 'tab-tiga', children = [
+            ## Add graph
+            html.P('Choose Rival : '),
+            html.Div(dcc.Input(id='rival-input', value='Malaysia', type='text')),
+            html.Div(html.Button('Search', id = 'rival-search'), style = {'margin-top' : '25px', 'margin-bottom' : '25px'}),
+            html.Div(id = 'rival-form', children = showrivaldataframe(football, 'Indonesia', 'Malaysia'))
+
         ]
         )
     ], content_style = {
@@ -57,19 +78,35 @@ app.layout = html.Div(children=[
 }
 )
 
-
+## Input awal,(pilih negara dan jumlah tahun)
 @app.callback(
-    Output(component_id = 'graph-scatter', component_property = 'figure'),
+    [Output(component_id = 'total-bar-chart', component_property = 'figure'),
+    Output(component_id = 'pie-chart', component_property = 'figure'),
+    Output(component_id = 'rival-chart', component_property = 'figure')],
     [Input(component_id = 'search', component_property = 'n_clicks')],
     [State(component_id = 'choose-country', component_property = 'value'),
     State(component_id = 'n-year', component_property = 'value')]
 )
 
-def show_bar_graph(n_clicks, x1, x2):
-    figure = matchresult(football, x1, x2)
-    return figure
+def show_graph(n_clicks, x1, x2):
+    bar_chart = matchresult(football, x1, x2)
+    pie_chart = homeawayrecord(football, x1, x2)
+    rival_bar = rivalresult(football, x1)
+    return bar_chart, pie_chart, rival_bar
 
 
+
+## Input kedua,(pilih negara rival)
+@app.callback(
+    Output(component_id = 'rival-form', component_property = 'children'),
+    [Input(component_id = 'rival-search', component_property = 'n_clicks')],
+    [State(component_id = 'choose-country', component_property = 'value'),
+    State(component_id = 'rival-input', component_property = 'value')]
+)
+
+def show_graph(n_clicks, x1, x2):
+    rival_table = showrivaldataframe(football, x1, x2)
+    return rival_table
 
 
 if __name__ == '__main__':
--- src/graph.py
+++ src/graph.py
@@ -5,8 +5,12 @@ import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output, State
+from datetime import datetime
+
+currentyear = datetime.now().year
 
 football = pd.read_csv('football.csv')
+football['date'] = pd.to_datetime(football['date'])
 
 def matchresult(result, country, year):
     country = country.capitalize()
@@ -30,3 +34,89 @@ def matchresult(result, country, year):
                 'layout': {'title': '{}\'s Match Result in last {} Years'.format(country, year), 'barmode' : 'stack'}
             }
     return figure
+
+
+def homeawayrecord(result, country, year):
+    global currentyear
+    country = country.capitalize()
+    negara = result[((result['home_team'] == country) | (result['away_team'] == country)) & (result['year'] > currentyear-year)]
+
+    negara['win'] = negara['who wins'].apply(lambda x : 1 if x == country else 0)
+    negara['draw'] = negara['who wins'].apply(lambda x : 1 if x == 'draw' else 0)
+    negara['lose'] = negara['who wins'].apply(lambda x : 1 if (x != country)&(x != 'draw') else 0)
+    
+    negaraHome = negara[(negara['home_team'] == country) & (negara['neutral'] == False)]
+    negaraNeutral = negara[negara['neutral'] == True]
+    negaraAway = negara[(negara['away_team'] == country) & (negara['neutral'] == False)]
+    
+    columnNegara = {'home' : negaraHome, 'neutral' : negaraNeutral, 'away' :negaraAway}
+    
+    traces = []
+    x = 0
+    for i in columnNegara.keys():
+        labels = ['win', 'draw',' lose']
+        values = [columnNegara[i]['win'].sum(),
+                 columnNegara[i]['draw'].sum(),
+                 columnNegara[i]['lose'].sum()]
+        domain = {'x' : [x , x+0.3], 'y' : [0.1 , 1] }
+        trace = go.Pie(labels = labels,
+                   values = values,
+                   domain = domain,
+                   hoverinfo = 'label+percent+name',
+                   sort = False, 
+                   title = i + ' record\n')
+        traces.append(trace)
+        x += 0.33
+        
+    figure = {
+        'data' : traces,
+        'layout' : {'title' : '{}\'s home-neutral-away record last {} year'.format(country, year), 'titlefont':{'size' : 20}}
+    }
+    return figure
+
+
+
+def rivalresult(result, country):
+    country = country.capitalize()
+    negara = result[((result['home_team'] == country) | (result['away_team'] == country))]
+    
+    negara['win'] = negara['who wins'].apply(lambda x : 1 if x == country else 0)
+    negara['draw'] = negara['who wins'].apply(lambda x : 1 if x == 'draw' else 0)
+    negara['lose'] = negara['who wins'].apply(lambda x : 1 if (x != country)&(x != 'draw') else 0)
+    
+    daftarRival = (negara['home_team'].value_counts() + negara['away_team'].value_counts()).sort_values(ascending = False)[1:6] 
+    listRival = []
+    for i in daftarRival.index:
+        rival = negara[(negara['home_team'] == i) | (negara['away_team'] == i)]
+        listRival.append([i,rival['win'].sum(),rival['draw'].sum(),rival['lose'].sum()])
+    
+    listRival = pd.DataFrame(listRival, columns = ['rival', 'win', 'draw','lose'])#.sort_values(by = 'win', ascending = False)   
+
+    figure = {
+                'data': [
+                    {'x': listRival['rival'], 'y': listRival['win'], 'type': 'bar', 'name': 'win'},
+                    {'x': listRival['rival'], 'y': listRival['draw'], 'type': 'bar', 'name': 'draw'},
+                    {'x': listRival['rival'], 'y': listRival['lose'], 'type': 'bar', 'name': 'lose'}
+                ],
+                'layout': {'title': 'Top 5 {}\'s Fiercest Rival\'s'.format(country), 'barmode' : 'stack'}
+            }
+    return figure
+
+def showrivaldataframe(result, country, rival):
+    country = country.capitalize()
+    rival = rival.capitalize()
+    negara = result[((result['home_team'] == country) | (result['away_team'] == country))]
+    negaraRival = negara[((negara['home_team'] == rival) | (negara['away_team'] == rival))].tail()
+    negaraRival = negaraRival[['date','home_team','home_score','away_score','away_team','tournament','city','country']]
+    negaraRival = negaraRival.sort_values(by = 'date', ascending = False)
+    tab = dash_table.DataTable(
+                id='form-table',
+                columns=[{"name": i, "id": i} for i in negaraRival.columns],
+                data=negaraRival.to_dict('records'),
+                page_action = 'native',
+                page_current = 0,
+                page_size = 10
+            )
+
+    return tab
+
